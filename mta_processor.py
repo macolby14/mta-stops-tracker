85b6b719d344fde3c59fd922f5c7f8ade4acc649
Mark Colby <macolby14@gmail.com>
2023-01-04 22:38:07 -0500

    clean - black formatting

--- mta_processor.py
+++ mta_processor.py
@@ -13,10 +13,13 @@ def read_gtfs_realtime(feed_url, api_key):
     response = requests.get(feed_url, headers=req_headers)
 
     if response.status_code != 200:
-        print(f"HTTP Response not 200, url:{response.request.url},status_code:{response.status_code}, reason:{response.reason}")
+        print(
+            f"HTTP Response not 200, url:{response.request.url},status_code:{response.status_code}, reason:{response.reason}"
+        )
         return
 
     return response.content
+
 
 # takes a FeedMessage and gets returns a list of stop_time_updates that stop at target
 def find_stop_on_ace(feed, target_stop_id):
@@ -29,14 +32,18 @@ def find_stop_on_ace(feed, target_stop_id):
         for stop_time_update in entity.trip_update.stop_time_update:
             if stop_time_update.stop_id == target_stop_id:
                 out.append(stop_time_update)
-    
+
     return out
+
 
 # takes list of stop_time_updates and returns next n stops
 def find_next_n_stop_times(stop_time_updates, n):
     stop_time_updates.sort(key=lambda update: update.arrival.time)
-    out = [datetime.fromtimestamp(update.arrival.time) for update in stop_time_updates[:n]]
+    out = [
+        datetime.fromtimestamp(update.arrival.time) for update in stop_time_updates[:n]
+    ]
     return out
+
 
 def find_times_to_next_stop(upcoming_stop_times):
     times_to_next_stop = []
@@ -47,17 +54,19 @@ def find_times_to_next_stop(upcoming_stop_times):
             continue
         delta = t - now
         times_to_next_stop.append(delta.seconds // 60)
-    
-    return times_to_next_stop
 
+    return times_to_next_stop
 
 
 def main():
     load_dotenv()
-    MTA_API_KEY=os.getenv("MTA_API_KEY")
-    A_C_E_URI=os.getenv("A_C_E_URI")
+    MTA_API_KEY = os.getenv("MTA_API_KEY")
+    A_C_E_URI = os.getenv("A_C_E_URI")
 
-    proto_res = read_gtfs_realtime("https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace", MTA_API_KEY)
+    proto_res = read_gtfs_realtime(
+        "https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs-ace",
+        MTA_API_KEY,
+    )
     if not proto_res:
         print("No response from api. Exiting")
         exit(1)
@@ -66,7 +75,7 @@ def main():
     feed.ParseFromString(proto_res)
 
     # TODO - Make it so we can just declare the station name and direction.
-    ace_stops = find_stop_on_ace(feed,"A44N")
+    ace_stops = find_stop_on_ace(feed, "A44N")
 
     upcoming_ace_stop_times = find_next_n_stop_times(ace_stops, 3)
     out = find_times_to_next_stop(upcoming_ace_stop_times)
--- test_mta_processor.py
+++ test_mta_processor.py
@@ -3,14 +3,15 @@ import mta_processor
 import gtfs_realtime_pb2
 from datetime import datetime, timedelta
 
+
 class TestMTAProcessor(unittest.TestCase):
     def test_find_stop_on_ace_empty(self):
-        no_stops = mta_processor.find_stop_on_ace(feed="",target_stop_id="A44N")
-        self.assertEqual(no_stops,[])
-    
+        no_stops = mta_processor.find_stop_on_ace(feed="", target_stop_id="A44N")
+        self.assertEqual(no_stops, [])
+
     def test_find_stop_on_ace_results(self):
         feed = gtfs_realtime_pb2.FeedMessage()
-        test_file = open("test-data/example-ace-results","rb")
+        test_file = open("test-data/example-ace-results", "rb")
         example_feed = test_file.read()
         test_file.close()
         feed.ParseFromString(example_feed)
@@ -18,16 +19,23 @@ class TestMTAProcessor(unittest.TestCase):
         self.assertEqual(len(seven_stops), 7)
 
         nextStops = mta_processor.find_next_n_stop_times(seven_stops, 3)
-        expectedNextStops = [datetime(2023, 1, 3, 20, 31, 13), datetime(2023, 1, 3, 20, 39, 27), datetime(2023, 1, 3, 20, 51)]
-        self.assertEqual(nextStops,expectedNextStops)
+        expectedNextStops = [
+            datetime(2023, 1, 3, 20, 31, 13),
+            datetime(2023, 1, 3, 20, 39, 27),
+            datetime(2023, 1, 3, 20, 51),
+        ]
+        self.assertEqual(nextStops, expectedNextStops)
 
     def test_find_times_to_next_stops(self):
         now = datetime.now()
-        mockNextStops = [now + timedelta(minutes=2, seconds=30), now + timedelta(minutes=10, seconds= 30), now - timedelta(minutes=2)]
+        mockNextStops = [
+            now + timedelta(minutes=2, seconds=30),
+            now + timedelta(minutes=10, seconds=30),
+            now - timedelta(minutes=2),
+        ]
         times_to_next_stop = mta_processor.find_times_to_next_stop(mockNextStops)
         self.assertEqual(times_to_next_stop, [2, 10])
-    
-    
+
     def test_find_times_to_next_stops_none(self):
         now = datetime.now()
         mockNextStops = [now - timedelta(minutes=2)]
@@ -39,7 +47,7 @@ class TestMTAProcessor(unittest.TestCase):
         mockNextStops = [now - timedelta(seconds=30)]
         times_to_next_stop = mta_processor.find_times_to_next_stop(mockNextStops)
         self.assertEqual(times_to_next_stop, [])
-    
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
