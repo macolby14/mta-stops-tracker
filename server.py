e47fd5cb05676c73d6f14afe5a5218066e2a5e3c
Mark Colby <macolby14@gmail.com>
2023-01-07 22:54:31 -0500

    add - return json for endpoint

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import mta_processor
+import json
 
 
 app = Flask(__name__)
@@ -7,4 +8,4 @@ app = Flask(__name__)
 
 @app.get("/stops")
 def stops():
-    return mta_processor.get_upcoming_ace_stop_times()
+    return json.dumps({"stops": mta_processor.get_upcoming_ace_stop_times()})
