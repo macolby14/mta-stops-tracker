ba6744f32e010f2955a3e63f8057e6a506695391
Mark Colby <macolby14@gmail.com>
2023-01-11 00:02:21 -0500

    add python to serve from react build path

--- server.py
+++ server.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 import mta_processor
 import json
+import os
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="mta-fe/build")
+
+
+# serve staticn react app
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def serve(path):
+    if path != "" and os.path.exists(app.static_folder + "/" + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, "index.html")
 
 
 @app.get("/stops")
