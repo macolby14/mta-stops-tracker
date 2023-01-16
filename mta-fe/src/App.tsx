266fbd6e74d4bba0eacbc44953337bcfb4b614c5
Mark Colby <macolby14@gmail.com>
2023-01-16 12:52:17 -0500

    cleanup - removed unused code

--- mta-fe/src/App.tsx
+++ mta-fe/src/App.tsx
@@ -1,22 +1,9 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import "./reset.css";
 
 function fetchNextStopTimes() {
     return [4, 10, 15, 20, 30, 40, 50, 60];
-}
-
-type VertifcalFlexContainerProps = {
-    children?: React.ReactNode;
-};
-
-type FlexItemProps = {
-    children?: React.ReactNode;
-};
-
-function FlexItem({ children }: FlexItemProps) {
-    return <div>{children}</div>;
 }
 
 function App() {
