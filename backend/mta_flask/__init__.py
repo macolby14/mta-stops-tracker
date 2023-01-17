6f6712340c1c7694859c4f0f97d3bb625bdcf963
Mark Colby <macolby14@gmail.com>
2023-01-17 17:03:15 -0500

    refactor - move app creation to mta_flask directory

---
+++ backend/mta_flask/__init__.py
@@ -0,0 +1,31 @@
+import os
+from . import mta_processor
+import json
+import os
+from dotenv import load_dotenv
+from flask import Flask
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+
+    if not load_dotenv():
+        print("No .env file exists. Required to run program")
+        exit(1)
+
+    @app.get("/api/stops")
+    def stops():
+        try:
+            nextTimes = mta_processor.get_upcoming_ace_stop_times()
+        except:
+            return json.dumps({"message": "internal error at GET /stops"}), 500
+
+        return json.dumps(
+            {
+                "line": "c",
+                "station": "clinton-washington",
+                "nextTimes": mta_processor.get_upcoming_ace_stop_times(),
+            }
+        )
+
+    return app
